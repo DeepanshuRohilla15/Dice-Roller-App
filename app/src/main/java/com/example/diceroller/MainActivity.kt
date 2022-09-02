1b01227d158565377f34bccd49f818e9b3d37331
DeepanshuRohilla15 <20001001027deepanshu@dcrustm.org>
2022-09-02 09:47:29 +0530

    Adding ImageView

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,7 +15,28 @@ class MainActivity : AppCompatActivity() {
         val  rollButton : Button = findViewById(R.id.roll_button)
         rollButton.text= "Let's Roll"
         rollButton.setOnClickListener{
-            Toast.makeText(this,"button clicked",Toast.LENGTH_SHORT).show()
+            rollDice()
         }
     }
+
+    private fun rollDice() {
+        val randomInt = Random().nextInt(6)+ 1
+
+        val drawableResource = when(randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        val diceImage : ImageView = findViewById(R.id.dice_image)
+        diceImage.setImageResource(drawableResource)
+
+
+
+
+
+    }
+
 }
